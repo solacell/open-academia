6ced05df3139be51df7260a51c574fb76917156e
meowweao <ganeshfalak01@gmail.com>
2023-12-01 01:40:14 +0530

    Add Login and Register Page

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts")
 
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+
+const session = require('express-session');
+
 const connectDB = require('./server/config/db');
 
 const app = express();
@@ -13,6 +18,22 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
+app.use(cookieParser());
+
+app.use(session(
+        {
+            secret: 'keyboard kat',
+            resave: false,
+            saveUninitialized: true,
+            store: MongoStore.create(
+                {
+                    mongoUrl: process.env.MONGODB_URI
+                }
+            )
+        }
+    )
+)
 
 app.use(express.static('public'))
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,74 @@
 let express = require('express');
 let router = express.Router();
 let Post = require('../models/Post');
+let User = require('../models/User');
+let bcrypt = require('bcrypt');
+let jwt = require('jsonwebtoken');
+
+const adminLayout = '../views/layouts/admin';
 
 router.get('/admin', async (req, res) => {
-    
+    try {
+        const locals = {
+            title: "Admin Page"
+        };
+
+        res.render('admin/index', {locals, layout: adminLayout});
+    } catch (error) {
+        
+    }
+})
+
+router.get('/register', async (req, res) => {
+    try {
+        const locals = {
+            title: "Register - NodeJS Blog"
+        };
+
+        res.render('admin/register', {locals, layout: adminLayout});
+    } catch (error) {
+        
+    }
+})
+
+router.post('/admin', async (req, res) => {
+    try {
+        
+        const {username, password} = req.body;
+
+        console.log("Username: " + username);
+        console.log("Password: " + password);
+
+        res.redirect('/');
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.post('/register', async (req, res) => {
+
+    try {
+
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 5);
+        
+        try {
+            
+            const user = await User.create( { username, password: hashedPassword } );
+            res.status(201).json({ message: 'User Created', user});
+
+        } catch (error) {
+            if (error.code === 11000) {
+                res.status(409).json({ message: "User already exits."});
+            } 
+            res.status(500).json({ message: 'Internal server error.'});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
 module.exports = router;
